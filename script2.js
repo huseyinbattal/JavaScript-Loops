55d0e72ad9b1e9b311f4875ba2ba79fe4e6729e0
huseyinbatta <huseyinbattal42@gmail.com>
2021-12-24 18:30:42 +0300

    yorum satırı iptal

--- script2.js
+++ script2.js
@@ -1,20 +1,20 @@
-// const myArray = [6, 19, 20, 5];
-// const yourArray = [19, 81, 5, 2];
-// for (let i = 0; i < myArray.length; i++) {
-//     for (let j = 0; j < yourArray.length; j++) {
-//         if (myArray[i] == yourArray[j]) {
+const myArray = [6, 19, 20, 5];
+const yourArray = [19, 81, 5, 2];
+for (let i = 0; i < myArray.length; i++) {
+    for (let j = 0; j < yourArray.length; j++) {
+        if (myArray[i] == yourArray[j]) {
 
-//             console.log(yourArray[j]);
-//         }
-//     }
+            console.log(yourArray[j]);
+        }
+    }
 
-// }
+}
 
-// let counterTwo = 1;
-// while (counterTwo < 4) {
-//     console.log(counterTwo);
-//     counterTwo++;
-// }
+let counterTwo = 1;
+while (counterTwo < 4) {
+    console.log(counterTwo);
+    counterTwo++;
+}
 
 let countString = '';
 let i = 0;
